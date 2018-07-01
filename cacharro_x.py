a0079e3d340e6b228a0320beff145abaae12edd1
Mateo RODRIGUEZ <mrodrigu@e3r8p11.42.fr>
2018-07-02 01:01:57 +0200

    fsfsdgs

--- cacharro_x.py
+++ cacharro_x.py
@@ -6,7 +6,7 @@
 #    By: mrodrigu <mrodrigu@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2018/06/24 11:56:09 by mrodrigu          #+#    #+#              #
-#    Updated: 2018/06/27 16:56:50 by mrodrigu         ###   ########.fr        #
+#    Updated: 2018/06/30 16:10:10 by mrodrigu         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -14,6 +14,7 @@ import json
 import requests
 
 PAGE_TOKEN = "EAACEdEose0cBAPJFHY4XoJ8RjZAPJ9uKKGZAOtZCFDyl0FSQIql3cHvJ68ysZALzVZBwpZBzId2ZAADYy8ghSNkoTybsqxDl0LZBpibBRWzjY29N2Omph4ibGGmXHEgZBvauuTZB2E00Vs0IarnFjLtiPdtwRQu0vVFZA9rXTZCzfZADtXceHHD8zR4fD02Js0k9ZAPUInsaEs4cZAxcgZDZD"
+PAGE_TOKEN_PERMANENT = "EAACmfVZAQJwABAKNWZACWIX1we5sVa0vAmkcKLB6zUulcyqoOk0F7gLnJrXyc1yQtIMtzyft45tjyIUQDvlAHdRm3l0pnQpNI3ybjahzZAbFqVGaoedKX7i49eBtRCNkF43QAZAz48R3KhepGYepuighRPYs459TTtIY9BBnZCAZDZD`1"
 USER_TOKEN_SHORT = "EAACmfVZAQJwABANUHIcDWWwdPz3X7kEun2V9uPZBfupOCtYEWLCBZBePlDVX2a1BEj8ZCK0CZBYutERZA7yWi1TxRkKZC0nP2zcKEkY2lj2TmUb8y5RSPNKGbanTAZB86ICBVANWSwDrpFv2EDUO3FBnF8D56LJZBfxsZBT6kkdCZC7e9VC8elYZCAKcGeStZAzRvrZCwsHoTsFyeLXwZDZD"
 USER_TOKEN_PERMANENT = "EAACmfVZAQJwABAGMyEquQWk6Hz3G7COhe4cvqobBJreZCZAZAHtY7QK9S8MRXDTqAxOc3psHBSsXg2sxQaufq37az4wD35mjH0nSeCQlad0C2BF6fjueTG0nmCR8oVDr1XaTYkIQ90DjRpw2VnWO3j1c9M7aCHRxQZBt6jgXvvgZDZD"
 PAGE_ID = "371121376687075"
@@ -33,12 +34,21 @@ def get_page_permanent_token(user_id, user_token_permanent = USER_TOKEN_PERMANEN
 	url = "https://graph.facebook.com/v3.0/{}/accounts?access_token={}".format(user_id, user_token_permanent)
 	return ((json.loads((requests.get(url)).content.decode("utf8"))))
 
+def get_user_page_id(user_id, user_token_permanent = USER_TOKEN_PERMANENT):
+	url = "https://graph.facebook.com/v3.0/{}/accounts?access_token={}".format(user_id, user_token_permanent)
+	data = (json.loads((requests.get(url)).content.decode("utf8")))
+	ids = []
+	for i in data["data"]:
+		ids.append(i["id"])
+	return (ids)
+
 # url = "https://graph.facebook.com/v3.0/{}/feed/?access_token={}".format(PAGE_ID, PAGE_TOKEN)
 # peticion de informacion
 # url = "https://graph.facebook.com/v3.0/{}/?access_token={}&fields=message".format(COMMENT_ID, PAGE_TOKEN)
 # carga en un json
-data = get_page_permanent_token(get_user_id(), USER_TOKEN_PERMANENT)
-print(data);
+data = get_user_page_id(get_user_id())
+for i in data:
+	print(i);
 # print("Al post:\n" + data["message"] + "\nLe han dado like:")
 # url = "https://graph.facebook.com/v3.0/{}/likes/?access_token={}".format(COMMENT_ID, PAGE_TOKEN)
 # data = json.loads((requests.get(url)).content.decode("utf8"))
